600f8b88278b39c6728725fc1cf6e352b8a7170e
Alexander Fateev <foggy1989@gmail.com>
2020-12-13 15:25:25 +0300

    Extending Pipeline subsystem Added Camera component Added StaticMesh component

--- data/scripts/systems/pipeline.py
+++ data/scripts/systems/pipeline.py
@@ -1,9 +1,37 @@
 import yagl as gl
 
 import e3d3
+import e3d3.core
+import e3d3.math
+import e3d3.events
+
+
+class Camera(e3d3.core.Component):
+    def __init__(self, entity):
+        super(Camera, self).__init__(entity)
+        self.fov = 80
+        self.near = 0.1
+        self.far = 500
+
+    def activate(self):
+        Pipeline.active_camera = self
+
+
+class StaticMesh(e3d3.core.Component):
+    def __init__(self, entity):
+        super(StaticMesh, self).__init__(entity)
+        self.mesh = None
+        self.material = None
+
+    def on_start(self):
+        e3d3.events.subscribe(self, 'pipeline.draw')
+
+    def on_clean(self):
+        e3d3.events.remove(self, 'pipeline.draw')
 
 
 class Pipeline(e3d3.core.System):
+    active_camera = None
 
     @classmethod
     def run(cls):
