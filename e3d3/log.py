6bc1be67b2dc5c59c6b29722af02cf146e8bd488
Alexander <foggy1989@gmail.com>
2021-01-09 21:09:24 +0500

    Fix log output and filters
    Log system has error at filter and logger creation method and in result not work.
    This change it for get correct behavior.

--- e3d3/log.py
+++ e3d3/log.py
@@ -8,23 +8,29 @@ FORMAT = logging.Formatter('%(asctime)s[%(levelname)s]: %(relpath)s, line %(line
 class RelpathFilter(logging.Filter):
     def filter(self, record):
         record.relpath = os.path.relpath(record.pathname)
+        return record
 
 
 def get(name):
-    return logging.getLogger(name)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.addFilter(RelpathFilter())
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    return logger
 
 
-file_handler = logging.FileHandler('e3d3.log', 'w')
+file_handler = logging.FileHandler('./e3d3.log', mode='w')
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(FORMAT)
 
 stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.DEBUG)
+stream_handler.setLevel(logging.INFO)
 stream_handler.setFormatter(FORMAT)
 
-logger = logging.getLogger(__name__)
-logger.addFilter(RelpathFilter())
+logger = logging.getLogger('E3D3')
 logger.setLevel(logging.DEBUG)
+logger.addFilter(RelpathFilter())
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
